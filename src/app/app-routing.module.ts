35bf64f8c7855938c129ee4628701963dc06acea
unknown <diego@aquino.site>
2020-06-12 02:23:08 -0500

    forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'client/register',
+loadChildren: () => import('./client/register/register.module').then(m => m.RegisterModule) },
+{ path: 'client', loadChildren: () => import('./client/login/login.module').then(m => m.LoginModule) },
+{ path: 'client/medical', loadChildren: () => import('./client/medical/medical.module').then(m => m.MedicalModule) },
+{path: '', redirectTo: 'client', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
